b00974684bbeccfa79cc0d0d18f68c96be7a338a
Ville Turku <ville.turku99@gmail.com>
2021-05-30 16:48:34 +0300

    Expressions in components

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+
+  return (
+    <div>
+      <p>Hello world it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+    </div>
+  )
+}
 
 export default App;
