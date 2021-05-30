5e3bca2edcf0970d1012338be1c386d1fea27d59
Ville Turku <ville.turku99@gmail.com>
2021-05-31 02:25:00 +0300

    Component refactoring

---
+++ part2/courseinformation/src/components/Content.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Part from './Part'
+
+const Content = ({ course }) => (
+    <div>
+        <Part part={course.parts[0]} />
+        <Part part={course.parts[1]} />
+        <Part part={course.parts[2]} />
+    </div>
+)
+
+export default Content
