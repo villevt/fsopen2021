2cc0c23fb69d7101b55343e47d98f675c7957d02
Ville Turku <ville.turku99@gmail.com>
2021-05-31 02:53:53 +0300

    Reduce sum

--- part2/courseinformation/src/components/Total.js
+++ part2/courseinformation/src/components/Total.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const Total = ({ course }) => {
-    const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+
+    const sum = course.parts.reduce((a, b) => {
+        a.exercises += b.exercises
+        return a
+    }).exercises
+    
     return(
-        <p>Number of exercises {sum}</p>
+        <p><b>total of {sum} exercises</b></p>
     ) 
 }
 
