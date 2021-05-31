e8884fb1da38a697b68fad1a8f27faf5621c4f24
Ville Turku <ville.turku99@gmail.com>
2021-06-01 00:40:42 +0300

    Basic error handling for feather, in case API fails

--- part2/dataforcountries/src/components/Weather.js
+++ part2/dataforcountries/src/components/Weather.js
@@ -9,26 +9,28 @@ const Weather = ({country}) => {
     const [weather, setWeather] = useState()
 
     useEffect(() => {
-        console.log("!")
         axios.get(`http://api.weatherstack.com/current?access_key=${weather_api_key}&query=${country.capital}`).then(response => {
-            console.log("...")
             setWeather(response.data)
         })
     }, [country])
 
     let display = ""
     if (weather) {
-        console.log(weather)
-        display = (
-            <div>
-                <h2>Weather in {country.capital}</h2>
-                <b>Temperature:</b> {weather.current.temperature} Celsius
-                <br/>
-                <img alt={weather.current.weather_descriptions} src={weather.current.weather_icons} width="100" height="100" />
-                <br/>
-                <b>Wind:</b> {weather.current.wind_speed} direction {weather.current.wind_dir}
-            </div>
-        )
+        if (!weather.current) {
+            display ="Error fetching weather data"
+        }
+        else {
+            display = (
+                <div>
+                    <h2>Weather in {country.capital}</h2>
+                    <b>Temperature:</b> {weather.current.temperature} Celsius
+                    <br/>
+                    <img alt={weather.current.weather_descriptions} src={weather.current.weather_icons} width="100" height="100" />
+                    <br/>
+                    <b>Wind:</b> {weather.current.wind_speed} direction {weather.current.wind_dir}
+                </div>
+            )
+        }
     }
 
     console.log(display)
