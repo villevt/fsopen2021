7aafe39c0f22cd60aa3c281d836a49c805aa4879
Ville Turku <ville.turku99@gmail.com>
2021-05-31 20:58:47 +0300

    Changed react key to reflect the person id instead of name

--- part2/phonebook/src/components/Numbers.js
+++ part2/phonebook/src/components/Numbers.js
@@ -6,7 +6,7 @@ const Numbers = ({persons}) => (
     <table>
         <tbody>
             {persons.sort((a, b) => a.name < b.name ? -1 : 1).map(person => 
-                <Number key={person.name} person={person} />
+                <Number key={person.id} person={person} />
             )}
         </tbody>
     </table>
