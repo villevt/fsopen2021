e1176f8b06177b31db04df4a7af14038fbb5384c
Ville Turku <ville.turku99@gmail.com>
2021-05-31 03:17:18 +0300

    Undo change (wrong folder), redone correctly

--- part2/courseinformation/src/App.js
+++ part2/courseinformation/src/App.js
@@ -1,24 +1,57 @@
-import React, { useState } from 'react'
+import React from 'react';
+
+import Course from './components/Course'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      ...
+      <h1>Web development curriculum</h1>
+      {courses.map(course => <Course course={course} key={course.id}/>)}
     </div>
   )
 }
