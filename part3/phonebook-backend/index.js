9e21823bc74538e434f1203e0ab7d393388b9f3b
Ville Turku <ville.turku99@gmail.com>
2021-06-01 22:27:55 +0300

    GET a single person

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -28,6 +28,17 @@ app.get("/api/persons", (request, response) => {
     response.json(persons)
 })
 
+app.get("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
 app.get("/info", (request, response) => {
     response.send(`Phonebook has info for ${persons.length} people<br/><br/>${new Date()}`)
 })
