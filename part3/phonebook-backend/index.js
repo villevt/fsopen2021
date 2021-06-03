9446ccb9d7a94fc8944b51ee05c1d366d5712b18
Ville Turku <ville.turku99@gmail.com>
2021-06-04 00:27:32 +0300

    Get people from DB

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,8 +1,9 @@
+require("dotenv").config()
 const express = require("express")
 const morgan = require("morgan")
-const cors = require("cors")
-const app = express()
+const Person = require("./models/person")
 
+const app = express()
 app.use(express.static("build"))
 
 morgan.token("content", (request, response) => {
@@ -12,31 +13,10 @@ app.use(morgan(":method :url :status :res[content-length] - :response-time ms :c
 
 app.use(express.json())
 
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-    }
-]
-
 app.get("/api/persons", (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.post("/api/persons", (request, response) => {
