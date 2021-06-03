4ac34a2c2f2b05883371ac7f4703d7d25c49ab27
Ville Turku <ville.turku99@gmail.com>
2021-06-04 00:09:15 +0300

    Command line backend

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log("Give password as argument")
+    process.exit(1)
+} else if (process.argv.length !== 3 && process.argv.length !== 5) {
+    console.log("Invalid arguments")
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://phonebook-admin:${password}@cluster0.elo10.mongodb.net/Phonebook?retryWrites=true&w=majority`
+
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true})
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model("Person", personSchema)
+
+if (process.argv.length == 5) {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4]
+    })
+
+    person.save().then(result => {
+        console.log(`Added ${process.argv[3]} number ${process.argv[4]} to phonebook`)
+        mongoose.connection.close()
+    })
+} else {
+    console.log("Phonebook:")
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+}
