e37d32193864511dd587d0e5879f6f97302d9224
Ville Turku <ville.turku99@gmail.com>
2021-06-05 00:50:45 +0300

    Test for favorite blogs

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -7,53 +7,73 @@ test("dummy returns one", () => {
   expect(result).toBe(1)
 })
 
+const blogs = [{
+  _id: "5a422aa71b54a676234d17f8",
+  title: "Go To Statement Considered Harmful",
+  author: "Edsger W. Dijkstra",
+  url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+  likes: 5,
+  __v: 0
+}, {
+  _id: "5a322aa71b54a676234d17f8",
+  title: "Go To Statement Considered Harmful Considered Harmful",
+  author: "Frank Rubin",
+  url: "https://web.archive.org/web/20090320002214/http://www.ecn.purdue.edu/ParaMount/papers/rubin87goto.pdf",
+  likes: 6,
+  __v: 0
+}, {
+  _id: "5a322aa71b54a476234d1768",
+  title: "Who even cares?",
+  author: "Me",
+  url: "https://localhost:3000",
+  likes: 2,
+  __v: 0
+}]
+
 describe("total likes", () => {
   test("of empty list is zero", () => {
-    const blogs = []
-  
-    const result = listHelper.totalLikes(blogs)
+    const result = listHelper.totalLikes([])
     expect(result).toBe(0)
   })
 
   test("when list has only one blog equals the likes of that", () => {
-    const blogs = [{
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 5,
-      __v: 0
-    }]
-  
-    const result = listHelper.totalLikes(blogs)
+    const result = listHelper.totalLikes([blogs[0]])
     expect(result).toBe(5)
   })
 
-  test("when list has only one blog equals the likes of that", () => {
-    const blogs = [{
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 5,
-      __v: 0
-    }, {
+  test("of a bigger list is calculated right", () => {
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(13)
+  })
+})
+
+describe("favorite blogs", () => {
+  test("of empty list is null", () => {
+    const result = listHelper.favoriteBlog([])
+    expect(result).toEqual(null)
+  })
+
+  test("when list has only one blog equals it", () => {
+    const result = listHelper.favoriteBlog([blogs[1]])
+    expect(result).toEqual({
       _id: "5a322aa71b54a676234d17f8",
       title: "Go To Statement Considered Harmful Considered Harmful",
       author: "Frank Rubin",
       url: "https://web.archive.org/web/20090320002214/http://www.ecn.purdue.edu/ParaMount/papers/rubin87goto.pdf",
       likes: 6,
       __v: 0
-    }, {
-      _id: "5a322aa71b54a476234d1768",
-      title: "Who even cares?",
-      author: "Me",
-      url: "https://localhost:3000",
-      likes: 2,
+    })
+  })
+
+  test("of a bigger list returns the right blog", () => {
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result).toEqual({
+      _id: "5a322aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful Considered Harmful",
+      author: "Frank Rubin",
+      url: "https://web.archive.org/web/20090320002214/http://www.ecn.purdue.edu/ParaMount/papers/rubin87goto.pdf",
+      likes: 6,
       __v: 0
-    }]
-  
-    const result = listHelper.totalLikes(blogs)
-    expect(result).toBe(13)
+    })
   })
 })
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,9 +1,9 @@
-const dummy = blogs => {
+const dummy = () => {
   return 1
 }
 
 const totalLikes = blogs => {
-  if (blogs.length == 0) {
+  if (blogs.length === 0) {
     return 0 
   } else {
     return(blogs
@@ -12,7 +12,18 @@ const totalLikes = blogs => {
   }
 }
 
+const favoriteBlog = blogs => {
+  if (blogs.length === 0) {
+    return null
+  } else if (blogs.length === 1) {
+    return blogs[0]
+  } else {
+    return blogs.sort((a, b) => b.likes - a.likes)[0]
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
