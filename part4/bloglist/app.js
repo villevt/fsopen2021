a64e5d881db9ca64a9fa3a777f38db3c20a28be8
Ville Turku <ville.turku99@gmail.com>
2021-06-04 21:55:37 +0300

    Completed refactoring app

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,30 +3,14 @@ const config = require("./utils/config")
 const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
-const Blog = require("./models/blog")
-const app = express()
+const blogsRouter = require("./controllers/blogs")
 
-app.use(cors())
-app.use(express.json())
+const app = express()
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
-app.get("/api/blogs", (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use(cors())
+app.use(express.json())
+app.use("/api/blogs", blogsRouter)
 
 module.exports = app
