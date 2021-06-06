9ae3f788d67cdad95b8ad3886111cdb491910afd
Ville Turku <ville.turku99@gmail.com>
2021-06-06 23:37:49 +0300

    express-async-errors

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,3 +1,4 @@
+require("express-async-errors")
 const blogsRouter = require("express").Router()
 const Blog = require("../models/blog")
 
@@ -6,38 +7,25 @@ blogsRouter.get("/", async (request, response) => {
   response.json(blogs)
 })
 
-blogsRouter.post("/", async (request, response, next) => {
+blogsRouter.post("/", async (request, response) => {
   const blog = new Blog(request.body)
   blog.likes = blog.likes || 0
 
-  try {
-    const result = await blog.save()
-    response.status(201).json(result)
-  } catch (exception) {
-    next(exception)
-  }
+  const result = await blog.save()
+  response.status(201).json(result)
 })
 
-blogsRouter.put("/:id", async (request, response, next) => {
+blogsRouter.put("/:id", async (request, response) => {
   const blog = {
     likes: request.body.likes
   }
-
-  try {
-    const result = await Blog.findByIdAndUpdate(request.params.id, blog, {context: "query", new: true, runValidators: true})
-    response.status(204).json(result)
-  } catch (exception) { 
-    next(exception)
-  }
+  const result = await Blog.findByIdAndUpdate(request.params.id, blog, {context: "query", new: true, runValidators: true})
+  response.status(204).json(result)
 })
 
-blogsRouter.delete("/:id", async (request, response, next) => {
-  try {
-    const result = await Blog.findByIdAndRemove(request.params.id)
-    response.status(204).json(result)
-  } catch (exception) {
-    next(exception)
-  }
+blogsRouter.delete("/:id", async (request, response) => {
+  const result = await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).json(result)
 })
 
 module.exports = blogsRouter
