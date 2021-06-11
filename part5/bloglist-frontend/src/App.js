ec1c8d3d4431be9bdcee7b084ca50a1921c49caf
Ville Turku <ville.turku99@gmail.com>
2021-06-11 12:38:25 +0300

    Login functionality

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react"
 import Blog from "./components/Blog"
+import Login from "./components/Login"
 import blogService from "./services/blogs"
+import loginService from "./services/login"
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,14 +15,32 @@ const App = () => {
     )  
   }, [])
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
+  const handleLogin = async (username, password) => {
+    try {
+      const user = await loginService.login({username, password})
+      setUser(user)
+    } catch {
+      console.log("Error logging in")
+    }
+  }
+
+  if (!user) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <Login handleLogin={handleLogin}/>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h2>Blogs</h2>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+    )
+  }
 }
 
 export default App
