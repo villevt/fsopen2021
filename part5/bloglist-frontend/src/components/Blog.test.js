059c176d897f1f58c01a3ed85b1cea2cff0c2102
Ville Turku <ville.turku99@gmail.com>
2021-06-11 22:35:50 +0300

    All info is displayed when view is pressed

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,26 +1,26 @@
 import React from "react"
 import "@testing-library/jest-dom/extend-expect"
-import { render } from "@testing-library/react"
+import { render, fireEvent } from "@testing-library/react"
 import Blog from "./Blog"
 
 describe("<Blog />", () => {
+  const user = {
+    username: "Herpderp"
+  }
+
+  const blog = {
+    title: "Nice Blog",
+    author: "Mrx. Nice Gul",
+    url: "myspace.com",
+    likes: 300,
+    user: user
+  }
+
+  const currentUsername="It doesn't matter"
+  const handleLike = jest.fn()
+  const handleRemove = jest.fn()
+
   test("renders title and author, but not url or likes by default", () => {
-    const user = {
-      username: "Herpderp"
-    }
-
-    const blog = {
-      title: "Nice Blog",
-      author: "Mrx. Nice Gul",
-      url: "myspace.com",
-      likes: 300,
-      user: user
-    }
-
-    const currentUsername="It doesn't matter"
-    const handleLike = jest.fn()
-    const handleRemove = jest.fn()
-  
     const component = render(
       <Blog blog={blog} currentUsername={currentUsername} handleLike={handleLike} handleRemove={handleRemove}/>
     )
@@ -38,6 +38,31 @@ describe("<Blog />", () => {
     )
 
     expect(component.container).not.toHaveTextContent(
+      "300"
+    )
+  })
+
+  test("all info is rendered when view is pressed", () => {
+    const component = render(
+      <Blog blog={blog} currentUsername={currentUsername} handleLike={handleLike} handleRemove={handleRemove}/>
+    )
+  
+    const view = component.getByText("view")
+    fireEvent.click(view)
+
+    expect(component.container).toHaveTextContent(
+      "Nice Blog"
+    )
+
+    expect(component.container).toHaveTextContent(
+      "Mrx. Nice Gul"
+    )
+
+    expect(component.container).toHaveTextContent(
+      "myspace.com"
+    )
+
+    expect(component.container).toHaveTextContent(
       "300"
     )
   })
