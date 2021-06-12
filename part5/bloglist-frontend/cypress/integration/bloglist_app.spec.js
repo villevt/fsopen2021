b2c5557ee4ca1457040387b1d8770fc4d5884df4
Ville Turku <ville.turku99@gmail.com>
2021-06-12 15:02:01 +0300

    Tests for checking form at start

--- part5/bloglist-frontend/cypress/integration/bloglist_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/bloglist_app.spec.js
@@ -1,10 +1,20 @@
-describe('Blog app', function() {
+describe("Blog app", function() {
   beforeEach(function() {
-    cy.request('POST', 'http://localhost:3003/api/testing/reset')
-    cy.visit('http://localhost:3000')
+    cy.request("POST", "http://localhost:3003/api/testing/reset")
+    cy.visit("http://localhost:3000")
   })
 
-  it('Login form is shown', function() {
-    cy.contains("")
+  it("Login form is shown", function() {
+    cy.get("form")
+      .should("contain", "Username")
+      .and("contain", "Password")
+
+    cy.get("form")
+      .find("input")
+      .should("be.visible")
+
+    cy.get("form")
+      .find("button")
+      .should("contain", "Login")
   })
 })
