c2acc48389e9855c1434157ae45a8eedf8b526ec
Ville Turku <ville.turku99@gmail.com>
2021-06-12 14:30:53 +0300

    Reset routes for DB in testing mode

--- part5/bloglist-backend/app.js
+++ part5/bloglist-backend/app.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose")
 const blogsRouter = require("./controllers/blogs")
 const usersRouter = require("./controllers/users")
 const loginRouter = require("./controllers/login")
+const testingRouter = require("./controllers/testing")
 const errorHandler = require("./utils/errorhandler")
 const tokenExtractor = require("./utils/token-extractor")
 const userExtractor = require("./utils/user-extractor")
@@ -20,6 +21,10 @@ app.use(tokenExtractor)
 app.use("/api/blogs", userExtractor, blogsRouter)
 app.use("/api/users", usersRouter)
 app.use("/api/login", loginRouter)
+if (process.env.NODE_ENV === "test") {
+  app.use("/api/testing", testingRouter)
+}
+
 app.use(errorHandler)
 
 module.exports = app
