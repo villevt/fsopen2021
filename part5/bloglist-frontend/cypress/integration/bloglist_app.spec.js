1eb228a871952aeed745fbe87764c7d91c1e21ce
Ville Turku <ville.turku99@gmail.com>
2021-06-12 15:28:59 +0300

    Login tests

--- part5/bloglist-frontend/cypress/integration/bloglist_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/bloglist_app.spec.js
@@ -1,6 +1,15 @@
 describe("Blog app", function() {
+  const user = {
+    username: "aaa",
+    password: "bbb",
+    name: "ccc"
+  }
+
   beforeEach(function() {
     cy.request("POST", "http://localhost:3003/api/testing/reset")
+
+    cy.request("POST", "http://localhost:3003/api/users", user)
+
     cy.visit("http://localhost:3000")
   })
 
@@ -16,5 +25,35 @@ describe("Blog app", function() {
     cy.get("form")
       .find("button")
       .should("contain", "Login")
+  })
+
+  describe("Login",function() {
+    it("succeeds with correct credentials", function() {
+      cy.get("form")
+        .find("input[name=\"Username\"]").type(user.username)
+
+      cy.get("form")
+        .find("input[name=\"Password\"]").type(user.password)
+
+      cy.get("form")
+        .find("button")
+        .click()
+      
+      cy.contains("Logged in")
+    })
+
+    it("fails with wrong credentials", function() {
+      cy.get("form")
+        .find("input[name=\"Username\"]").type("wrongname")
+
+      cy.get("form")
+        .find("input[name=\"Password\"]").type("wrongpw")
+
+      cy.get("form")
+        .find("button")
+        .click()
+      
+      cy.contains("Invalid username or password")
+    })
   })
 })
