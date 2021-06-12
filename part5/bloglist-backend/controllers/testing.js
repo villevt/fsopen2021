8d11100fe24a77c00bf9ee727ef3ce7b8682b0bd
Ville Turku <ville.turku99@gmail.com>
2021-06-12 16:51:57 +0300

    Changed testing route to init db

--- part5/bloglist-backend/controllers/testing.js
+++ part5/bloglist-backend/controllers/testing.js
@@ -1,10 +1,9 @@
 const router = require("express").Router()
-const Blog = require("../models/blog")
-const User = require("../models/user")
+const helper = require("../tests/test_helper")
 
 router.post("/reset", async (request, response) => {
-  await Blog.deleteMany({})
-  await User.deleteMany({})
+  await helper.initializeUsers()
+  await helper.initializeBlogs()
 
   response.status(204).end()
 })
