44c642c0c74b1cd48564e3f56f4b191c3c50592a
Ville Turku <ville.turku99@gmail.com>
2021-06-14 19:02:56 +0300

    Functionality to add anecdotes

--- part6/anecdotes/src/App.js
+++ part6/anecdotes/src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { useSelector, useDispatch } from "react-redux"
-import { voteAnecdote } from "./reducers/anecdoteReducer"
+import { voteAnecdote, addAnecdote } from "./reducers/anecdoteReducer"
 
 const App = () => {
   const anecdotes = useSelector(state => state)
@@ -8,6 +8,11 @@ const App = () => {
 
   const vote = (id) => {
     dispatch(voteAnecdote(id))
+  }
+
+  const handleForm = event => {
+    event.preventDefault()
+    dispatch(addAnecdote(event.target.anecdote.value))
   }
 
   return (
@@ -25,9 +30,9 @@ const App = () => {
         </div>
       )}
       <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
+      <form onSubmit={handleForm}>
+        <div><input name="anecdote"/></div>
+        <button type="submit">create</button>
       </form>
     </div>
   )
