5f56fc5260dfb9377420488f7cca09954f1885d9
Ville Turku <ville.turku99@gmail.com>
2021-06-14 19:12:20 +0300

    AnecdoteForm as separate component

---
+++ part6/anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { useDispatch } from "react-redux"
+import { addAnecdote } from "../reducers/anecdoteReducer"
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch()
+
+  const handleForm = event => {
+    event.preventDefault()
+    dispatch(addAnecdote(event.target.anecdote.value))
+  }
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={handleForm}>
+        <div>
+          <input name="anecdote"/>
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
