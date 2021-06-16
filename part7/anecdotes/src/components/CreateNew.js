98ab2d06fd7227d39dc9c158c4b9566739f96508
Ville Turku <ville.turku99@gmail.com>
2021-06-16 13:19:45 +0300

    Spread syntax workaround for "reset" on inputs

--- part7/anecdotes/src/components/CreateNew.js
+++ part7/anecdotes/src/components/CreateNew.js
@@ -39,15 +39,15 @@ const CreateNew = ({addNew}) => {
         <form onSubmit={handleSubmit}>
           <div>
             content
-            <input {...content} />
+            <input {...{...content, reset: null}} />
           </div>
           <div>
             author
-            <input {...author} />
+            <input {...{...author, reset: null}} />
           </div>
           <div>
             url for more info
-            <input {...info} />
+            <input {...{...info, reset: null}} />
           </div>
           <button>create</button>
           <button onClick={handleReset}>reset</button>
