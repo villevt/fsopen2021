5029bf97ae0882728d94e7134a9b1436e3a1fbec
Ville Turku <ville.turku99@gmail.com>
2021-06-18 15:41:10 +0300

    Navigation styling

--- part7/bloglist-frontend/src/components/Navigation.js
+++ part7/bloglist-frontend/src/components/Navigation.js
@@ -1,35 +1,75 @@
 import React from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { Link } from "react-router-dom"
+import styled from "styled-components"
+
 import { logoutUser } from "../reducers/currentUser"
+
+const Container = styled.div`
+  background-color: Sienna;
+  color: Snow;
+  display: flex;
+  padding: 1em;
+  > a {
+    border-color: Snow;
+    border-style: solid;
+    border-width: 1px;
+    border-radius: 5px;
+    color: Snow;
+    text-decoration: none;
+    padding: 0.25em;
+    margin-left: 0.5em;
+    margin-right: 0.5em;
+  }
+  > a:hover {
+    background-color: Chocolate
+  }
+`
+
+const User = styled.div`
+  align-self: center;
+  margin-left: 70%;
+  > button {
+    background: none;
+    border-color: Snow;
+    border-style: solid;
+    border-width: 1px;
+    border-radius: 5px;
+    color: Snow;
+    text-decoration: none;
+    padding: 0.25em;
+    margin-left: 0.5em;
+    margin-right: 0.5em;
+    font-size: 1em;
+  }
+  > button:hover {
+    background-color: Chocolate
+  }
+`
 
 const Navigation = () => {
   const dispatch = useDispatch()
   const user = useSelector(state => state.currentUser)
-
-  const navigationStyle = {
-    backgroundColor: "lightgray",
-    padding: "10px",
-    display: "flex"
-  }
 
   const handleLogout = () => {
     dispatch(logoutUser())
   }
 
   return (
-    <div style={navigationStyle}>
+    <Container>
       <Link to="/blogs">
         Blogs
       </Link>
       <Link to="/users">
         Users
       </Link>
-      {user.name} logged in 
-      <button onClick={() => handleLogout()}>
-        Logout
-      </button>
-    </div>
+      <User>
+        {user.name} logged in 
+        <button onClick={() => handleLogout()}>
+          Logout
+        </button>
+      </User>
+    </Container>
   )
 }
 
