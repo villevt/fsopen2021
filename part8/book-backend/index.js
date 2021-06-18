074f861a4cb332f544e141489be86f8e6157e5b6
Ville Turku <ville.turku99@gmail.com>
2021-06-18 22:29:38 +0300

    allAuthors query support

--- part8/book-backend/index.js
+++ part8/book-backend/index.js
@@ -79,6 +79,11 @@ let books = [
 ]
 
 const typeDefs = gql`
+  type Author {
+    name: String!
+    bookCount: Int!
+  }
+
   type Book {
     title: String!
     author: String!
@@ -90,6 +95,7 @@ const typeDefs = gql`
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -97,7 +103,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors
+  },
+
+  Author: {
+    bookCount: root => books.filter(book => book.author == root.name).length
   }
 }
 
