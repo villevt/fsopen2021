07618fc605994cc909052092e7f4635e9a040ec6
Ville Turku <ville.turku99@gmail.com>
2021-06-29 22:20:47 +0300

    Express, GET /hello

---
+++ part9/ts-projects/index.ts
@@ -0,0 +1,11 @@
+import express from "express"
+
+const app = express()
+
+app.get("/hello", (_req, res) => {
+  res.send("Hello Full Stack!")
+})
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000")
+})
