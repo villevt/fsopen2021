66790a984d2181f829d89565f862e993134f5a78
Ville Turku <ville.turku99@gmail.com>
2021-07-02 23:25:24 +0300

    Entries in backend

--- part9/patientor-backend/services/patientService.ts
+++ part9/patientor-backend/services/patientService.ts
@@ -1,16 +1,16 @@
 import {v4 as uuid} from "uuid";
 
-import { Gender, Patient, NewPatient } from "../types";
-import patientData from "../data/patients.json";
+import { Patient, NewPatient } from "../types";
+import patientData from "../data/patients";
 
-const patients: Patient[] = patientData.map(({id, name, dateOfBirth, ssn, gender, occupation}) => ({
+const patients: Patient[] = patientData.map(({id, name, dateOfBirth, ssn, gender, occupation, entries}) => ({
   id,
   name,
   dateOfBirth,
   ssn,
-  gender: gender as Gender,
+  gender,
   occupation,
-  entries: []
+  entries
 }));
 
 const getAll = (): Omit<Patient, "ssn">[] => {
--- part9/patientor-backend/types.ts
+++ part9/patientor-backend/types.ts
@@ -10,9 +10,48 @@ export enum Gender {
   Other = "other"
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose['code']>;
 }
+
+export type Discharge = {
+  date: string,
+  criteria: string
+};
+
+export interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge
+}
+
+export type SickLeave = {
+  startDate: string,
+  endDate: string
+};
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export type Entry = HospitalEntry | OccupationalHealthcareEntry | HealthCheckEntry;
 
 export type Patient = {
   id: string,
