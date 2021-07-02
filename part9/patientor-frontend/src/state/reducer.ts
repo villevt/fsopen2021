0691f46f1d61fcb2f50e885fe9b59af44fd6a573
Ville Turku <ville.turku99@gmail.com>
2021-07-02 23:07:46 +0300

    Action creator functions

--- part9/patientor-frontend/src/state/reducer.ts
+++ part9/patientor-frontend/src/state/reducer.ts
@@ -37,7 +37,6 @@ export const reducer = (state: State, action: Action): State => {
         }
       };
     case "UPDATE_PATIENT":
-      console.log("heh");
       return {
         ...state,
         patients: {
@@ -48,4 +47,25 @@ export const reducer = (state: State, action: Action): State => {
     default:
       return state;
   }
+};
+
+export const setPatientList = (data: Patient[]): Action => {
+  return {
+    type: "SET_PATIENT_LIST",
+    payload: data
+  };
+};
+
+export const addPatient = (data: Patient): Action => {
+  return {
+    type: "ADD_PATIENT",
+    payload: data
+  };
+};
+
+export const updatePatient = (data: Patient): Action => {
+  return {
+    type: "UPDATE_PATIENT",
+    payload: data
+  };
 };
