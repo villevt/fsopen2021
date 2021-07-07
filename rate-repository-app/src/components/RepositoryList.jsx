8c5a26b11e051917dcfcaac43abe5ddf27752dc0
Ville Turku <ville.turku99@gmail.com>
2021-07-07 17:03:52 +0300

    useRepositories hook

--- rate-repository-app/src/components/RepositoryList.jsx
+++ rate-repository-app/src/components/RepositoryList.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { FlatList, View, StyleSheet} from "react-native";
 
+import useRepositories from '../hooks/useRepositories';
 import { RepositoryItem } from "./RepositoryItem";
 
 const styles = StyleSheet.create({
@@ -10,23 +11,8 @@ const styles = StyleSheet.create({
 });
 
 const RepositoryList = () => {
-  const [repositories, setRepositories] = useState();
+  const repositories = useRepositories;
 
-  const fetchRepositories = async () => {
-    // Replace the IP address part with your own IP address!
-    const response = await fetch('http://192.168.100.16:5000/api/repositories');
-    const json = await response.json();
-
-    console.log(json);
-
-    setRepositories(json);
-  };
-
-  useEffect(() => {
-    fetchRepositories();
-  }, []);
-
-  // Get the nodes from the edges array
   const repositoryNodes = repositories
     ? repositories.edges.map(edge => edge.node)
     : [];
