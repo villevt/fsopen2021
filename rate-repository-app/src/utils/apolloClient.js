b2842eef00085b7e3b3310cb33351ff72c1c2610
Ville Turku <ville.turku99@gmail.com>
2021-07-08 21:12:21 +0300

    Apollo URI as env variable

--- rate-repository-app/src/utils/apolloClient.js
+++ rate-repository-app/src/utils/apolloClient.js
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import Constants from 'expo-constants';
 
 const httpLink = createHttpLink({
-  uri: 'http://192.168.43.137:5000/graphql',
+  uri: Constants.manifest.extra.APOLLO_URI,
 });
 
 const createApolloClient = () => {
