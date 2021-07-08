118a0bc8bdff90b1233eb45c02a90389a2d4b683
Ville Turku <ville.turku99@gmail.com>
2021-07-08 20:46:16 +0300

    cache-and-network fetch policy

--- rate-repository-app/src/hooks/useRepositories.js
+++ rate-repository-app/src/hooks/useRepositories.js
@@ -2,7 +2,7 @@ import { useQuery } from '@apollo/client';
 import { GET_REPOSITORIES } from '../graphql/queries';
 
 const useRepositories = () => {
-  const {loading, data, refetch} = useQuery(GET_REPOSITORIES);
+  const {loading, data, refetch} = useQuery(GET_REPOSITORIES, {fetchPolicy: 'cache-and-network'});
 
   return { repositories: data ? data.repositories: {}, loading, refetch };
 };
